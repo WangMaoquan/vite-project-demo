9a23821dfe4cf72be486cd805dc213b592e81d3c
wangmaoquan <1782976211@qq.com>
2022-12-12 13:20:14 +0800

    feat: 使用vite css 的配置为每个scss文件自动导入全局变量的scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import { normalizePath } from 'vite';
+
+const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.join(__dirname, 'src'),
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
 });
