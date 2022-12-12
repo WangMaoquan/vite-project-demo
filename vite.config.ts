7eb9da7853df6db1ffcdaf66d7985332a309f5a9
wangmaoquan <1782976211@qq.com>
2022-12-12 14:47:43 +0800

    style: 代码格式修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import viteEslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.join(__dirname, 'src'),
-  plugins: [react()],
+  plugins: [react(), viteEslint()]
 });
