28524f3fb13a9085e627d7fd8e839aecae453ac0
wangmaoquan <1782976211@qq.com>
2022-12-13 14:19:40 +0800

    feat: vite 配置其他静态资源

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       '@assets': path.join(__dirname, 'src/assets')
     }
-  }
+  },
+  /**
+   * Vite 也对下面几类格式提供了内置的支持
+   * 媒体类文件，包括mp4、webm、ogg、mp3、wav、flac和aac
+   * 字体类文件。包括woff、woff2、eot、ttf 和 otf
+   * 文本类。包括webmanifest、pdf和txt
+   * 都可以当做一个es模块
+   * 如果你的项目中还存在其它格式的静态资源，你可以通过assetsInclude配置让 Vite 来支持加载
+   */
+  assetsInclude: ['.gltf']
 });
