2bfac5a7362b61797f2ab50df5f09ff80224f3c3
wangmaoquan <1782976211@qq.com>
2022-12-13 13:50:27 +0800

    feat: viteconfig添加配置, 并在tsconfig增加types

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import viteEslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.join(__dirname, 'src'),
-  plugins: [react(), viteEslint()],
+  plugins: [react(), viteEslint(), svgr()],
   resolve: {
     alias: {
       '@assets': path.join(__dirname, 'src/assets')
